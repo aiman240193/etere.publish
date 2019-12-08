56e56905bf2d4cee4a19299cfc44d63c3facca8e
aiman2401 <aiman240193@gmail.com>
2019-12-08 14:11:06 +0100

    initial commit

---
+++ resources/views/layout/partials/header.blade.php
@@ -0,0 +1,10 @@
+<section class="jumbotron text-center">
+    <div class="container">
+        <h1 class="jumbotron-heading">etere.publish</h1>
+        <p class="lead text-muted">5G, IOT, SDN</br> L'avanguardia delle tecnologie wireless mobile.</p>
+        <p>
+            <a href="#" class="btn btn-primary">Consulta</a>
+            <a href="#" class="btn btn-secondary">Pubblica</a>
+        </p>
+    </div>
+</section>
---
+++ resources/views/layout/partials/nav.blade.php
@@ -0,0 +1,48 @@
+<!--Navbar -->
+<nav class="mb-1 navbar navbar-expand-lg navbar-light secondary-color lighten-1">
+    <a class="navbar-brand" href="#">e.p</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-555"
+            aria-controls="navbarSupportedContent-555" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent-555">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Chi Siamo
+                    <span class="sr-only">(current)</span>
+                </a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-555" data-toggle="dropdown"
+                   aria-haspopup="true" aria-expanded="false">Sezioni
+                </a>
+                <div class="dropdown-menu dropdown-secondary" aria-labelledby="navbarDropdownMenuLink-555">
+                    <a class="dropdown-item" href="#">5G</a>
+                    <a class="dropdown-item" href="#">IOT</a>
+                    <a class="dropdown-item" href="#">SDN</a>
+                </div>
+            </li>
+        </ul>
+        <ul class="navbar-nav ml-auto nav-flex-icons">
+            <li class="nav-item">
+                <a class="nav-link waves-effect waves-light">1
+                    <i class="fas fa-envelope"></i>
+                </a>
+            </li>
+            <li class="nav-item avatar dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-55" data-toggle="dropdown"
+                   aria-haspopup="true" aria-expanded="false">
+                    <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-2.jpg" class="rounded-circle z-depth-0"
+                         alt="avatar image">
+                </a>
+                <div class="dropdown-menu dropdown-menu-lg-right dropdown-secondary"
+                     aria-labelledby="navbarDropdownMenuLink-55">
+                    <a class="dropdown-item" href="#">Il mio profilo</a>
+                    <a class="dropdown-item" href="#">Pubblica</a>
+                    <a class="dropdown-item" href="#">I miei articoli</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
+<!--/.Navbar -->
